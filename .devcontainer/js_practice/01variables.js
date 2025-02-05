50985815ab7bcebbf96fa41a8fad72581e5ba19d
Indrayani Bhosale <142092078+indrayanibhosale08@users.noreply.github.com>
2025-02-05 06:05:36 +0000

    add files

---
+++ .devcontainer/js_practice/01variables.js
@@ -0,0 +1,19 @@
+const accId = 2020;
+
+let accEmail = "aaa1@gmail.com";
+let password = "a123";
+let accCity = "Jaipur";
+
+var b ="qqq";
+
+//accId = 3233;
+/* prefer not to  use var for issue in 
+block scope and functional scope*/ 
+
+let accountState;
+
+accEmail = "abc1@gmail.com";
+console.log(accEmail);
+
+console.table([accId,accCity,accEmail,password,accountState]);
+
---
+++ .devcontainer/js_practice/02datatypes.js
@@ -0,0 +1,30 @@
+"use strict" //treat all the JS code as newer version
+
+//alert(3+3);
+
+console.log("hjcv");
+
+console.log(3+3);
+ 
+console.log("Indrayani");
+ let name = "Indrayani";
+ let age = 18;
+ let isLogged = true ;
+ 
+ //2 to  power 53
+ //BigInt
+ //Boolean => true/false
+ //null => standalone value
+ //undefined
+ //number
+ //symbol => unique
+
+
+ //object
+  let state = null ;
+
+  console.log(typeof null);
+  console.log(typeof undefined);
+  console.log(typeof age);
+
+  
